49a16722771790a13cfd7f534a3956d3b5f71302
Duc Tang <02022003nha@gmail.com>
2024-10-05 13:28:03 +0700

    Nhan vien

---
+++ DATA/HeThong/mapTaiKhoanAdmin.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DATA.Entity;
+
+namespace DATA.HeThong
+{
+    public class mapTaiKhoanAdmin : DBConfig
+    {
+        public List<TAIKHOANADMIN> Danhsach()
+        {
+            try
+            {
+                return db.TAIKHOANADMIN.ToList();
+            }
+            catch (Exception)
+            {
+
+                return new List<TAIKHOANADMIN>();
+            }
+        }
+        public TAIKHOANADMIN ChiTiet(string user, string pass)
+        {
+            try
+            {
+                var data = db.TAIKHOANADMIN.SingleOrDefault(m => m.TENDANGNHAP.ToLower() == user.ToLower() & m.MATKHAU == pass);
+                return data;
+
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+    }
+}
---
+++ QLNhanSu/Controllers/NhanvienController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+namespace QLNhanSu.Controllers
+{
+    public class NhanvienController : Controller
+    {
+        // GET: Nhanvien
+        public ActionResult Nhanvien()
+        {
+            return View();
+        }
+
+        
+    }
+}
