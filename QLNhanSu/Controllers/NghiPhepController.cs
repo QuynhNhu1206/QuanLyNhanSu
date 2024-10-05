796210020b66780a0c9f975a5b3359d41a3d3f67
unknown <vovankhoi25102003@gmail.com>
2024-10-05 13:45:58 +0700

    Nghi phep

---
+++ QLNhanSu/Controllers/NghiPhepController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNhanSu.Controllers
+{
+    public class NghiPhepController : Controller
+    {
+        // GET: NghiPhep
+        public ActionResult NghiPhep()
+        {
+            return View();
+        }
+    }
+}
