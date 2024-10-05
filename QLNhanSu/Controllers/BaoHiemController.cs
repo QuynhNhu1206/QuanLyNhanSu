1211e9b9a02c70882af4cdb4939a0474396605d7
trantthungan22 <02022003nha@gmail.com>
2024-10-05 12:46:51 +0700

    BH,HD

---
+++ QLNhanSu/Controllers/BaoHiemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNhanSu.Controllers
+{
+    public class BaoHiemController : Controller
+    {
+        // GET: BaoHiem
+        public ActionResult BaoHiem()
+        {
+            return View();
+        }
+        public ActionResult BaoHiemYTE()
+        {
+            return View();
+        }
+        public ActionResult BaoHiemXH()
+        {
+            return View();
+        }
+    }
+}
---
+++ QLNhanSu/Controllers/HopDongController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNhanSu.Controllers
+{
+    public class HopDongController : Controller
+    {
+        // GET: HopDong
+        public ActionResult HopDong()
+        {
+            return View();
+        }
+    }
+}
