f7020367539886be2bc00d380bd7abe0a16577dc
Phuong <kingkuty724@gmail.com>
2024-10-05 12:59:20 +0700

    PB

--- DATA/Entity/QLNS.Designer.cs
+++ DATA/Entity/QLNS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\TMDTCODE\QLNhanSu\DATA\Entity\QLNS.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\NNKP2507\Documents\QuanLyNhanSu\DATA\Entity\QLNS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ QLNhanSu/Controllers/PhongBanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLNhanSu.Controllers
+{
+    public class PhongBanController : Controller
+    {
+        // GET: PhongBan
+        public ActionResult PhongBan()
+        {
+            return View();
+        }
+    }
+}
